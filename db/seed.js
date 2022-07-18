71fee847c7456bec7e64cd7ea6895719b835d6dd
Darius Robinson <robinson.darius090@gmail.com>
2022-07-18 13:40:38 -0400

    working on the get table post section

--- db/seed.js
+++ db/seed.js
@@ -1,22 +1,21 @@
 // grab our client with destructuring from the export in index.js
-const { client, getAllUsers, createUser } = require("./index");
-
+const { client, getAllUsers, createUser, updateUser } = require("./index");
 
 async function dropTables() {
-    try {
-        console.log("Starting to drop tables...");
-        
-        await client.query(`
+  try {
+    console.log("Starting to drop tables...");
+
+    await client.query(`
         DROP TABLE IF EXISTS users;
         `);
-        
-        console.log("Finished dropping tables!");
-    } catch (error) {
-        console.error("Error dropping tables!");
-        throw error; // we pass the error up to the function that calls dropTables
-        }
-        }
-        async function testDB() {
+
+    console.log("Finished dropping tables!");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error; // we pass the error up to the function that calls dropTables
+  }
+}
+async function testDB() {
   try {
     // connect the client to the database, finally
     client.connect();
@@ -33,15 +32,12 @@ async function dropTables() {
   }
 }
 
-
-
-
 // this function should call a query which creates all tables for our database
 async function createTables() {
-    try {
-        console.log("Starting to build tables...");
-        
-        await client.query(`
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
         CREATE TABLE users (
             id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
@@ -51,66 +47,91 @@ async function createTables() {
             active BOOLEAN DEFAULT true
             );
             `);
-            console.log("Finished building tables!");
-            
-        } catch (error) {
-            console.error("Error building tables!");
-            throw error; // we pass the error up to the function that calls createTables
-        }
-    }
-      
-      // new function, should attempt to create a few users
-      async function createInitialUsers() {
-        try {
-          console.log("Starting to create users...");
-      
-          const albert = await createUser({ username: 'albert', password: 'bertie99', name:'Al Bert',location:'Sidney, Australia' });
-          const sandra = await createUser({ username: 'sandra', password: '2sandy4me',name:'Just Sandra',location: "Ain't tellin" });
-          const glamgal = await createUser({ username: 'glamgal', password: 'soglam' ,name:'Joshua',location:'Upper East Side'});
-          console.log(albert );
-          console.log(sandra);
-          console.log(glamgal);
-      
-          console.log("Finished creating users!");
-        } catch(error) {
-          console.error("Error creating users!");
-          throw error;
-        }
-      }
+    await client.query(`
+           CREATE TABLE posts (
+              id SERIAL PRIMARY KEY,
+              "authorID" INTEGER REFERENCES users(id) NOT NULL,
+              title VARCHAR(255) NOT NULL,
+              content TEXT NOT NULL,
+              active BOOLEAN DEFAULT true
+           );
+           `);
+    console.log("Finished building tables!");
+  } catch (error) {
+    console.error("Error building tables!");
+    throw error; // we pass the error up to the function that calls createTables
+  }
+}
 
+// new function, should attempt to create a few users
+async function createInitialUsers() {
+  try {
+    console.log("Starting to create users...");
 
-    async function rebuildDB() {
-        try {
-            client.connect();
-            
-            await dropTables();
-            await createTables();
-            await createInitialUsers();
+    const albert = await createUser({
+      username: "albert",
+      password: "bertie99",
+      name: "Al Bert",
+      location: "Sidney, Australia",
+    });
+    const sandra = await createUser({
+      username: "sandra",
+      password: "2sandy4me",
+      name: "Just Sandra",
+      location: "Ain't tellin",
+    });
+    const glamgal = await createUser({
+      username: "glamgal",
+      password: "soglam",
+      name: "Joshua",
+      location: "Upper East Side",
+    });
+    console.log(albert);
+    console.log(sandra);
+    console.log(glamgal);
 
-        } catch (error) {
-            console.error(error);
-        }
-    }
+    console.log("Finished creating users!");
+  } catch (error) {
+    console.error("Error creating users!");
+    throw error;
+  }
+}
 
-    async function testDB() {
-      try {
-        console.log("Starting to test database...");
-        
-       
-        // queries are promises, so we can await them
-        const users = await getAllUsers();
-        // for now, logging is a fine way to see what's up
-        console.log("getAllUsers:", users);
-        
-        console.log("Finished database tests!");
-      } catch (error) {
-        console.error("Error testing database!");
-        console.error(error);
-      }
-    }
-    
-    rebuildDB()
-    .then(testDB)
-    .catch(console.error)
-    .finally(() => client.end());
-    
+async function rebuildDB() {
+  try {
+    client.connect();
+
+    await dropTables();
+    await createTables();
+    await createInitialUsers();
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function testDB() {
+  try {
+    console.log("Starting to test database...");
+
+    console.log("Calling getAllUsers");
+    const users = await getAllUsers();
+    console.log("Result:", users);
+
+    console.log("Calling updateUser on users[0]");
+    const updateUserResult = await updateUser(users[0].id, {
+      name: "Newname Sogood",
+      location: "Lesterville, KY",
+    });
+    console.log("Result:", updateUserResult);
+
+    console.log("Finished database tests!");
+  } catch (error) {
+    console.error("Error testing database!");
+    throw error;
+  }
+}
+
+rebuildDB()
+  .then(testDB)
+  .catch(console.error)
+  .finally(() => client.end());
