4713d6e812ac3172b72b406c138cce17782c36dd
Darius Robinson <robinson.darius090@gmail.com>
2022-07-20 16:42:48 -0400

    trying to fix bug in userRouter.post

--- api/users.js
+++ api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const usersRouter = express.Router();
 const { getAllUsers } = require('../db');
-
+const {getUserByUsername} = require('../db')
 
 
 usersRouter.use((req, res, next) => {
@@ -17,34 +17,40 @@ usersRouter.get('/', async (req, res) => {
       users
     });
   });
+
   usersRouter.post('/login', async (req, res, next) => {
-    const { username, password } = req.body;
-  
-    // request must have both
-    if (!username || !password) {
-      next({
-        name: "MissingCredentialsError",
-        message: "Please supply both a username and password"
-      });
-    }
-  
-    try {
-      const user = await getUserByUsername(username);
-  
-      if (user && user.password == password) {
-        // create token & return to user
-        res.send({ message: "you're logged in!" });
-      } else {
-        next({ 
-          name: 'IncorrectCredentialsError', 
-          message: 'Username or password is incorrect'
-        });
-      }
-    } catch(error) {
-      console.log(error);
-      next(error);
-    }
+    console.log(req.body);
+    res.end();
   });
+
+  // usersRouter.post('/login', async (req, res, next) => {
+  //   const { username, password } = req.body;
+  
+  //   // request must have both
+  //   if (!username || !password) {
+  //     next({
+  //       name: "MissingCredentialsError",
+  //       message: "Please supply both a username and password"
+  //     });
+  //   }
+  
+  //   try {
+  //     const user = await getUserByUsername(username);
+  
+  //     if (user && user.password == password) {
+  //       // create token & return to user
+  //       res.send({ message: "you're logged in!" });
+  //     } else {
+  //       next({ 
+  //         name: 'IncorrectCredentialsError', 
+  //         message: 'Username or password is incorrect'
+  //       });
+  //     }
+  //   } catch(error) {
+  //     console.log(error);
+  //     next(error);
+  //   }
+  // });
 
 
 
