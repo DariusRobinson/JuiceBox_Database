6d2ef44d3cef9af178d09691f6cfb191a79b46ee
Darius Robinson <robinson.darius090@gmail.com>
2022-07-21 17:52:44 -0400

    fixed the post section

--- api/posts.js
+++ api/posts.js
@@ -44,13 +44,12 @@ postsRouter.use((req, res, next) => {
          // if the post comes back, res.send({ post });
          // otherwise, next an appropriate error object 
         
-         const { authorId, title, content }=postData
+        
+//puting these in objects
 
-        // postData={
-        // authorId,
-        // title,
-        // content
-        // }
+        postData.authorId = req.user.id
+        postData.title = title
+        postData.content= content
 
         const post = await createPost(postData);
 
@@ -58,43 +57,7 @@ postsRouter.use((req, res, next) => {
         res.send({ post });
       }
       
-      // usersRouter.post('/register', async (req, res, next) => {
-      //   const { username, password, name, location } = req.body;
-      
-      //   try {
-      //     const _user = await getUserByUsername(username);
-      
-      //     if (_user) {
-      //       next({
-      //         name: 'UserExistsError',
-      //         message: 'A user by that username already exists'
-      //       });
-      //     }
-      
-      //     const user = await createUser({
-      //       username,
-      //       password,
-      //       name,
-      //       location,
-      //     });
-      
-      //     const token = jwt.sign({ 
-      //       id: user.id, 
-      //       username
-      //     }, process.env.JWT_SECRET, {
-      //       expiresIn: '1w'
-      //     });
-      
-      //     res.send({ 
-      //       message: "thank you for signing up",
-      //       token 
-      //     });
-      //   } catch ({ name, message }) {
-      //     next({ name, message })
-      //   } 
-      // });
-
-
+    
        
       } catch ({ name, message }) {
         next({ name, message: "No post to display" });
