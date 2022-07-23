0858d0c519b59374ba39226730d99ba16b364a6a
Darius Robinson <robinson.darius090@gmail.com>
2022-07-23 15:46:06 -0400

    finished the project

--- api/tags.js
+++ api/tags.js
@@ -1,46 +1,45 @@
-const express = require('express');
+const express = require("express");
 const tagsRouter = express.Router();
-const { getAllTags, getPostsByTagName } = require('../db');
-
-
+const { getAllTags, getPostsByTagName } = require("../db");
 
 tagsRouter.use((req, res, next) => {
-    console.log("A request is being made to /tags");
-  
-    next(); 
-  });
-  
+  console.log("A request is being made to /tags");
 
-  tagsRouter.get('/:tagName/posts', async (req, res, next) => {
-    const {tagName} = req.params;
-    // read the tagname from the params
-    
-    try {
-    
-      // let newTagName = decodeURIComponent(tagName)
-        const postsByTags = await getPostsByTagName(tagName);
-        console.log (postsByTags, "this is post by tag")
+  next();
+});
 
-         console.log(tagName, "newtagname")
-        //  tagNamePosts.posts = posts
-        res.send({postsByTags: postsByTags})
-      // use our method to get posts by tag name from the db
-      // send out an object to the client { posts: // the posts }
-    } catch ({ name, message }) {
-      // forward the name and message to the error handler
-      next ({ name, message })
-    }
-  });
+tagsRouter.get("/:tagName/posts", async (req, res, next) => {
+  const { tagName } = req.params;
+  // read the tagname from the params
 
-  tagsRouter.get('/', async (req, res) => {
-      const tags = await getAllTags();
+  try {
+    // let newTagName = decodeURIComponent(tagName)
+    const postsByTags = await getPostsByTagName(tagName);
     
-      res.send({
-        tags
-      });
+    console.log(postsByTags, "this is post by tag");
+
+    // adding filter to  filter out any posts which are both inactive and not owned by the current user...
+    const visiblePosts = postsByTags.filter(post => {
+      return post.active || (req.user && post.author.id === req.user.id);
     });
-  
-  
 
+    console.log(tagName, "newtagname");
+    
+    res.send({ postsByTags: postsByTags });
+    // use our method to get posts by tag name from the db
+    // send out an object to the client { posts: // the posts }
+  } catch ({ name, message }) {
+    // forward the name and message to the error handler
+    next({ name, message });
+  }
+});
+
+tagsRouter.get("/", async (req, res) => {
+  const tags = await getAllTags();
+
+  res.send({
+    tags,
+  });
+});
 
 module.exports = tagsRouter;
